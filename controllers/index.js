5ea578d03276b679f1eed86c545012ee8fc28c26
TristanRice <tristan@24marketing.co.uk>
2019-01-31 17:07:41 +0000

    Added navbar

--- controllers/index.js
+++ controllers/index.js
@@ -5,7 +5,7 @@ const express     = require("express")
     , router      = express.Router( );
 
 router.get("/", (req, res) => {
-    res.render("index");
+    res.render("main/index");
 });
 
 router.use("/", auth);
---
+++ public/scripts/index/index.js
@@ -0,0 +1,10 @@
+$(function( ) {
+	$(document).ready(function( ) {
+		$(".dropdown-trigger").dropdown( );
+	});
+
+	$("#main_nav").hover(function( ) {
+		//here we should fade in the text
+		
+	});
+});
