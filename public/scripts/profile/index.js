0558e17d588d65bb368f44e44aa0f23825564eb5
min0r2468 <minor_2468@hotmail.com>
2019-02-06 22:20:08 +0000

    Started doing frontend chekcs on user's input

--- public/scripts/profile/index.js
+++ public/scripts/profile/index.js
@@ -16,7 +16,7 @@ $(function( ) {
               </div>
               <div class="col s3">
                 <!--group price-->
-                <label for="hotel_room_group_price">Group price</label>
+                <label for="hotel_room_group_price">Group price (per room)</label>
                 <input class="validate" placeholder="Group price" id="hotel_room_group_price" type="text" required="true"/>
               </div>
               <div class="col s3">
@@ -31,13 +31,92 @@ $(function( ) {
         });
     });
 
+
+    function showErrorMessage(message, id) {
+        console.log(id);
+        $(`#${id}`).html(" "+message);
+    }
+
+    let Form_1 = {
+        input_IDs: {
+            "hotel_name": "(Hotel name must be between 2 and 20 characters)",
+            "hotel_phone_number": "(Phone number is not valid)",
+            "hotel_country": "(This is not a valid country)",
+            "hotel_city": "(City name must be between 1 and 100 characters)",
+            "hotel_postcode": "(Postcode must be between 1 and 30 characters)",
+            "hotel_full_address": "(Address must be between 1 and 500 characters)",
+        },
+
+        tooltip_messages: [
+            "Hotel name must be between 2 and 20 characters",
+            "Phone number must be valid",
+            false,
+            false,
+            false,
+            "Your address will be shown on your profile"
+        ],
+
+        makeErrorMessage: function(value, min, max, id) {
+            console.log(value.length);
+            if (!value.length || value.length < min || value.length > max){
+                showErrorMessage(Form_1.input_IDs[id], `${id}_error`);
+                console.log(id);
+            }
+        },
+
+        validate: function( ) {
+            let valid = true;
+            Object.keys(Form_1.input_IDs).forEach(function(input_id) {
+                let value = $(`#${input_id}`).val( );
+                switch(input_id) {
+
+                    case "hotel_name":
+                        Form_1.makeErrorMessage(value, 2, 20, input_id)
+                        break;
+
+                    case "hotel_phone_number":
+                        let regex = /^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){0,})(?:[\-\.\ \\\/]?(?:#|ext\.?|extension|x)[\-\.\ \\\/]?(\d+))?$/i;
+                        if (!value.match(regex) || !value.length)
+                            showErrorMessage(Form_1.input_IDs[input_id], input_id+"_error");
+                        break;
+
+                    case "hotel_country":
+                        break;
+
+                    case "hotel_city":
+                        Form_1.makeErrorMessage(value, 1, 100, input_id);
+                        break;
+
+                    case "hotel_postcode":
+                        Form_1.makeErrorMessage(value, 1, 30, input_id);
+                        break;
+
+                    case "hotel_full_address":
+                        Form_1.makeErrorMessage(value, 1, 500, input_id);
+                        break;
+
+                    default:
+                        valid = false;
+                        break;
+                }
+            });
+            return valid;
+        }
+    };
+
+    function verifyInput(form="") {
+        return Form_1.validate( ) || $(".invalid").length;
+    }
+
     $("#submit_1").click(function( ) {
+        if (verifyInput( )) return false;
         $("#add_hotel_1").fadeOut(function( ) {
             $("#add_hotel_2").fadeIn( );
         });
     });
 
     $("#submit_2").click(function( ) {
+        if (verifyInput( )) return false;
         $("#add_hotel_2").fadeOut(function( ){
             $("#add_hotel_3").fadeIn( );
         })
